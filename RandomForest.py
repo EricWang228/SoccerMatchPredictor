bcdc497190b363c2e8d3cf2abe60c6dc9de3d42e
EricWang228 <99090141+EricWang228@users.noreply.github.com>
2024-07-20 10:14:01 -0400

    Finished naive model implementation

--- RandomForest.py
+++ RandomForest.py
@@ -2,7 +2,9 @@ import csv
 import pandas as pd
 import os
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score 
+from sklearn.metrics import precision_score
+
 def clean_data() -> list:
     premier_matches = pd.read_csv(os.getcwd() + "/webscrape/premier_league_2023-2024.csv")
     
@@ -12,7 +14,6 @@ def clean_data() -> list:
     
     # Convert columns to numeric data values for ML algorithms
     premier_matches["Date"] = pd.to_datetime(premier_matches["Date"])
-    
     premier_matches['target'] = premier_matches["Result"].astype('category').cat.codes
     
     # The venue type (home/away) and opponent can be a strong predictor of a team's performance
@@ -20,20 +21,32 @@ def clean_data() -> list:
     premier_matches['op_codes'] = premier_matches['Opponent'].astype('category').cat.codes
     
     return premier_matches
-def model(data : list):
+
+def predict(premier_matches : list):
+    # Create a Random Forest Model
     rf = RandomForestClassifier(n_estimators=100, min_samples_split=10, random_state=1)
-    train = data
+    # Predictors and training data
     predictors = ["venue_code", "op_codes", "Poss", "xG", "xGA"]
-    rf.fit(train[predictors], train["target"])
-    preds = rf.predict(data[predictors])
-    error = accuracy_score(data["target"], preds)
-    combined = pd.DataFrame(dict(actual=data["target"], predicted=preds))
-    print(pd.crosstab(index=combined["actual"], columns=combined["predicted"]))
-    # print(combined)
+    train_set = premier_matches[premier_matches['Date'] < '2023-12-28']
+    test_set = premier_matches[premier_matches['Date'] > '2023-12-28']
+    
+    # Fit the random forest model
+    rf.fit(train_set[predictors], train_set['target'])
+    prediction = rf.predict(test_set[predictors])
+    
+    # Compare the fitted model with the actual results of the game
+    compared = pd.DataFrame(dict(actual=test_set['target'], predicted=prediction), index=test_set.index)
+    
+    # Compute the accuracy of the model with the actual results
+    error = accuracy_score(test_set['target'], prediction)
+    return compared, error
+    
     
 def main():
-    cleaned_matches = clean_data()
-    model(cleaned_matches)
+    premier_matches = clean_data()
+    predictions, err = predict(premier_matches)
+    print(err)
+    print(predictions)
     
 if __name__ == '__main__':
     main()
